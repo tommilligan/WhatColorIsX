ef397a0722fe4820e12ddcf3ae44ef907169eef8
Tom Milligan <tommilligan@users.noreply.github.com>
2015-10-09 22:56:49 +0100

    Tidied up, added argparse and example

--- src/WhatColorIsX.py
+++ src/WhatColorIsX.py
@@ -1,14 +1,10 @@
 from PIL import Image
+import argparse
 import urllib
 import urllib2
 import simplejson
 import cStringIO
-
-def rgb2hex(rgb): # Takes rbg, returns hex - http://stackoverflow.com/questions/3380726/converting-a-rgb-color-tuple-to-a-six-digit-code-in-python
-    def clamp(x): 
-        return max(0, min(x, 255))
-    hex = "#{0:02x}{1:02x}{2:02x}".format(clamp(rgb[0]), clamp(rgb[1]), clamp(rgb[2]))
-    return hex
+from colour import Color
 
 def average_image_color(i): # Takes image, returns rgb - http://stackoverflow.com/questions/29726148/finding-average-color-using-python
 	h = i.histogram()
@@ -32,6 +28,7 @@ def get_image(searchTerm): # Takes string, returns image - http://stackoverflow.
     searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + urllib.quote_plus(searchTerm) + "&start=" + str(startIndex)
     f = fetcher.open(searchUrl)
     deserialized_output = simplejson.load(f)
+    # 4 image URLs returned - take first one that doesn't IOerror and is RGB(A) format
     for i in range(4):
         imageUrl = deserialized_output['responseData']['results'][i]['unescapedUrl']
         try:
@@ -39,22 +36,21 @@ def get_image(searchTerm): # Takes string, returns image - http://stackoverflow.
             img = Image.open(file)
         except IOError:
             continue
-        
         if (img.mode == "RGB" or img.mode == "RGBA"):
             return img
     return False
 
-def what_color(searchTerm): # Takes string, returns hex
+def what_color(searchTerm, bright_hue=False): # Takes string, returns hex
     img = get_image(searchTerm)
     avg = average_image_color(img)
-    hex = rgb2hex(avg)
-    return hex
-
+    color = Color(rgb=[float(avg[x])/255.0 for x in range(3)])
+    if bright_hue is True:
+        color = Color(hue=color.hue, saturation=1.0, luminance=0.5)
+    return color.hex
     
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) > 1:
-        print what_color(sys.argv[1])
-    else:
-        print 'usage: WhatColorIsX.py STRING'
-        print 'prints the average color of STRING as hex value as determined by a Google image search.'
+    parser = argparse.ArgumentParser(description='Returns colour of string based on Google image search.')
+    parser.add_argument('string', help='string to find colour of')
+    parser.add_argument('-b', '--bright_hue', action='store_true', help='return a bright colour; hsl=(x,1.0,0.5)')
+    args = parser.parse_args()
+    print what_color(args.string, bright_hue=args.bright_hue)
--- src/xmlInterface.py
+++ src/xmlInterface.py
@@ -1,34 +1,31 @@
 import WhatColorIsX
 import xml.etree.ElementTree as ET
-import os
-from colour import Color
+import argparse
 
-def update_xml(xml_path, element_tag, name_tag, color_tag):
+def update_xml(xml_path, save_path, element_tag, name_tag, color_tag, bright_hue=False):
     tree = ET.parse(xml_path)
     root = tree.getroot()
     try:
         for element in root.findall(element_tag):
             ename = element.find(name_tag).text
-            ehex = WhatColorIsX.what_color(ename) #Get color from google
+            ehex = WhatColorIsX.what_color(ename, bright_hue=bright_hue) #Get color from google
+            ecolor = ET.SubElement(element, color_tag)
             
-            #Optional color alteration
-            eColor = Color(ehex)
-            altColor = Color(hue=eColor.hue, saturation=1.0, luminance=0.5)
-            ehex = altColor.hex
-            
-            ecolor = element.find(color_tag)
             ecolor.text = ehex
             print ename, ehex
-            #raise MemoryError #debug first col only
     finally:
-        new_path = 'elements.xml'
-        tree.write(new_path)
-        return 'XML saved to '+new_path
+        new_path = 'output.xml'
+        tree.write(save_path)
+        return 'XML saved to '+save_path
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) > 1:
-        print update_xml(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-    else:
-        print 'usage: xmlinterface.py xml_path element_tag name_tag color_tag'
-        print 'updates the xml file at xml_path for element_tag with color_tag as determined by a Google image search for name_tag.'
+    parser = argparse.ArgumentParser(description='Look up multiple string colors using xml')
+    parser.add_argument('xml_path', help='input xml file')
+    parser.add_argument('save_path', help='output xml file')
+    parser.add_argument('parent_element', help='the element under which color will be saved')
+    parser.add_argument('name_element', help='the value in each name_element will be Google searched')
+    parser.add_argument('color_element', help='the element under which color will be saved')
+    parser.add_argument('-b', '--bright_hue', action='store_true', help='return a bright colour; hsl=(x,1.0,0.5)')
+    args = parser.parse_args()
+    print update_xml(args.xml_path, args.save_path, args.parent_element, args.name_element, args.color_element, bright_hue=args.bright_hue)    
+    
